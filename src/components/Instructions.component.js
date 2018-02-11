d8782b005baffe568921f5e80c72193abd690dbe
Alex <awu1337@gmail.com>
2018-02-11 01:28:19 -0600

    Part 1 Complete

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
-// TODO: uncomment this for Part 1
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
+  static propTypes = {
+    shouldDisplayImage: PropTypes.bool.isRequired
+  }
   render() {
     return (
       <div className="instructions">
         Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
+        {this.props.shouldDisplayImage && (
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        )}
       </div>
     )
   }
