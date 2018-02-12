a9275efe22c9dbe30df9a7abe36897ca7c5b7f23
Alex <awu1337@gmail.com>
2018-02-12 00:03:01 -0600

    Part 5 complete

--- src/App.js
+++ src/App.js
@@ -11,18 +11,52 @@ class Counter extends Component {
     super(props)
     this.state = { count: this.props.initialCount }
   }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      count: nextProps.initialCount
+    })
+  }
   incClick = () => {
     this.setState(state => ({
       count: state.count + 1
     }))
   }
+  decClick = () => {
+    this.setState(state => ({
+      count: state.count - 1
+    }))
+  }
+  render() {
+    return (
+      <div>
+        Count state: {this.state.count}
+        <button onClick={this.decClick}>Decrease</button>
+        <button onClick={this.incClick}>Increase</button>
+      </div>
+    )
+  }
+}
+
+class InitialCountForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      newCount: 0
+    }
+  }
   handleSubmit(event) {
     this.setState({
-      count: Number(this.initialCount)
+      count: Number(this.state.newCount),
+      newCount: this.state.newCount
     })
+    this.props.appCallback(Number(this.state.newCount))
   }
   handleForm(event) {
-    this.initialCount = event.target.value
+    this.setState({
+      count: Number(this.state.count),
+      newCount: event.target.value
+    })
   }
   render() {
     return (
@@ -38,20 +72,30 @@ class Counter extends Component {
         <button type="button" onClick={this.handleSubmit.bind(this)}>
           Submit this
         </button>
-        <br />
-        Count state: {this.state.count}
-        <button onClick={this.incClick}>Increase</button>
       </div>
     )
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { initialCount: 0 }
+  }
+  countCallback = newCount => {
+    this.setState({
+      initialCount: newCount
+    })
+  }
   render() {
+    let count = this.state.initialCount
     return (
       <div className="app">
         <h1>Hello World!</h1>
-        <Counter initialCount={0} />
+        <p>
+          <Counter initialCount={count} />
+          <InitialCountForm appCallback={this.countCallback} />
+        </p>
         <Instructions
           shouldDisplayImage={true}
           items={['first', 'second', 'third', 'fourth', 'gg', 'asdf', '12']}
