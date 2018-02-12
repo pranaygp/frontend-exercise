c3c6d54e07afec0a65c26c31e6905a0dcd4ed06b
shreyshrey1 <talk2shreyas@gmail.com>
2018-02-12 00:59:28 -0600

    part one done

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-// TODO: uncomment this for Part 1
-// import PropTypes from 'prop-types'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
